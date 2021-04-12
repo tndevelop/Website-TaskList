a2bec6866021dc20715f517d3c70c94982a9b92e
Luca Orlando <s281933@studenti.polito.it>
2021-04-12 20:52:12 +0200

    Update App.js

--- to_do_list/src/App.js
+++ to_do_list/src/App.js
@@ -1,25 +1,39 @@
+/*<<<<<<< Updated upstream
 import logo from "./logo.svg";
 import "./App.css";
+=======
 
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Container, Row, Button, ListGroup, Navbar } from 'react-bootstrap';
+>>>>>>> Stashed changes
+*/
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Container, Row, Button, ListGroup, Navbar } from 'react-bootstrap';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row className="vheight-100">
+        <Col sm={4} xs={12} className="bg-light below-nav d-sm-block collapse" id="left-sidebar">
+          <ListGroup variant="flush">
+            <ListGroup.Item active action href="#">All</ListGroup.Item>
+            <ListGroup.Item action href="#">Important</ListGroup.Item>
+            <ListGroup.Item action href="#">Today</ListGroup.Item>
+            <ListGroup.Item action href="#">Next 7 Days</ListGroup.Item>
+            <ListGroup.Item action href="#">Private</ListGroup.Item>
+          </ListGroup> 
+          
+        </Col>
+      </Row>
+    </Container>
+     
   );
 }
+
+
+
 
 export default App;
