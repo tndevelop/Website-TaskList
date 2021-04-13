02470f24f8416bf2276cdd3e2b9f5a5d2fe8429e
Daniele R.C <darthreca@gmail.com>
2021-04-13 21:20:05 +0200

    Template for inserting navbar and side

--- to_do_list/src/App.js
+++ to_do_list/src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import { Form, ListGroup, Container, Col, Row, Image } from "react-bootstrap";
+import { Form, ListGroup, Container, Col, Row } from "react-bootstrap";
 import { PersonSquare } from "react-bootstrap-icons";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { FaTrashAlt, FaPencilAlt } from "react-icons/fa";
@@ -8,71 +8,79 @@ import "./App.css";
 
 function App() {
   return (
-    <Container fluid="xl" className="mainContainer">
-      <h1 id="selectedFilter">
-        <b>Filter</b>: all
-      </h1>
-      <ListGroup variant="flush">
-        {/* Task 1 */}
-        <ListGroup.Item>
-          <Row>
-            <Col>
-              <Form.Check type="checkbox" label="Study for the exam" />
-            </Col>
+    <Container fluid>
+      <Row>Navn</Row>
+      <Row>
+        <Col sm="auto">Side</Col>
+        <Col as={Container} fluid="xl" className="mainContainer">
+          <h1 id="selectedFilter">
+            <b>Filter</b>: all
+          </h1>
+          <ListGroup variant="flush">
+            {/* Task 1 */}
+            <ListGroup.Item>
+              <Row>
+                <Col>
+                  <Form.Check type="checkbox" label="Study for the exam" />
+                </Col>
 
-            <Col sm="auto">
-              <FaPencilAlt className="pencil"></FaPencilAlt>
-            </Col>
+                <Col sm="auto">
+                  <FaPencilAlt className="pencil"></FaPencilAlt>
+                </Col>
 
-            <Col sm={0.5}>
-              <FaTrashAlt className="trash"></FaTrashAlt>
-            </Col>
-          </Row>
-        </ListGroup.Item>
-        {/* Taks2 */}
-        <ListGroup.Item>
-          <Row>
-            <Col>
-              <Form.Check
-                type="checkbox"
-                label="Prepare the slides for the exam"
-              />
-            </Col>
-            <Col sm="2">
-              <PersonSquare></PersonSquare>
-            </Col>
+                <Col sm={0.5}>
+                  <FaTrashAlt className="trash"></FaTrashAlt>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            {/* Taks2 */}
+            <ListGroup.Item>
+              <Row>
+                <Col>
+                  <Form.Check
+                    type="checkbox"
+                    label="Prepare the slides for the exam"
+                  />
+                </Col>
+                <Col sm="2">
+                  <PersonSquare></PersonSquare>
+                </Col>
 
-            <Col>Saturday, June 20th 2020, 12:00:00 am</Col>
+                <Col>Saturday, June 20th 2020, 12:00:00 am</Col>
 
-            <Col sm="auto">
-              <FaPencilAlt className="pencil"></FaPencilAlt>
-            </Col>
+                <Col sm="auto">
+                  <FaPencilAlt className="pencil"></FaPencilAlt>
+                </Col>
 
-            <Col sm="0.5">
-              <FaTrashAlt className="trash"></FaTrashAlt>
-            </Col>
-          </Row>
-        </ListGroup.Item>
-        {/* Task 3 */}
-        <ListGroup.Item>
-          <Row>
-            <Col>
-              <Form.Check type="checkbox" label="Call Mary" />
-            </Col>
+                <Col sm="0.5">
+                  <FaTrashAlt className="trash"></FaTrashAlt>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            {/* Task 3 */}
+            <ListGroup.Item>
+              <Row>
+                <Col>
+                  <Form.Check type="checkbox" label="Call Mary" />
+                </Col>
 
-            <Col sm="auto">
-              <FaPencilAlt className="pencil"></FaPencilAlt>
-            </Col>
+                <Col sm="auto">
+                  <FaPencilAlt className="pencil"></FaPencilAlt>
+                </Col>
 
-            <Col sm={0.5}>
-              <FaTrashAlt className="trash"></FaTrashAlt>
-            </Col>
-          </Row>
-        </ListGroup.Item>
-      </ListGroup>
-      {/* Bottom plus */}
-
-      <AiFillPlusCircle className="plusButton" color="green"></AiFillPlusCircle>
+                <Col sm={0.5}>
+                  <FaTrashAlt className="trash"></FaTrashAlt>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+          </ListGroup>
+          {/* Bottom plus */}
+          <AiFillPlusCircle
+            className="plusButton"
+            color="green"
+          ></AiFillPlusCircle>
+        </Col>
+      </Row>
     </Container>
   );
 }
