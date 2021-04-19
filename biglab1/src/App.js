713bf185c729742b9f5cf436d29d6a6e5e6db9dc
JonnyMarsiano <jonnymarsiano@gmail.com>
2021-04-19 11:18:06 +0200

    creazione componente MainContent

--- biglab1/src/App.js
+++ biglab1/src/App.js
@@ -3,12 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Container, Row, Navbar, Form, ListGroup } from "react-bootstrap";
 import FormControl from 'react-bootstrap/FormControl'
 import React from "react";
-import { AiFillPlusCircle } from "react-icons/ai";
-import { FaTrashAlt, FaPencilAlt } from "react-icons/fa";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BsCheckAll } from "react-icons/bs";
 import { BiUserCircle } from "react-icons/bi";
+import MainContent from "./components/MainContent";
 
 function App() {
   return (
@@ -43,74 +42,8 @@ function App() {
             </ListGroup>
        </Col>
 
-        <Col as={Container} fluid="xl" className="mainContainer below-nav">
-          <h1 id="selectedFilter">
-            <b>Filter</b>: all
-          </h1>
-          <ListGroup variant="flush">
-            {/* Task 1 */}
-            <ListGroup.Item>
-              <Row>
-                <Col>
-                  <Form.Check type="checkbox" label="Study for the exam" />
-                </Col>
-
-                <Col sm="auto">
-                  <FaPencilAlt className="pencil"></FaPencilAlt>
-                </Col>
-
-                <Col sm={0.5}>
-                  <FaTrashAlt className="trash"></FaTrashAlt>
-                </Col>
-              </Row>
-            </ListGroup.Item>
-            {/* Taks2 */}
-            <ListGroup.Item>
-              <Row>
-                <Col>
-                  <Form.Check
-                    type="checkbox"
-                    label="Prepare the slides for the exam"
-                  />
-                </Col>
-                <Col sm="2">
-                  <BiUserCircle size="1.85em"></BiUserCircle>
-                </Col>
-
-                <Col>Saturday, June 20th 2020, 12:00:00 am</Col>
-
-                <Col sm="auto">
-                  <FaPencilAlt className="pencil"></FaPencilAlt>
-                </Col>
-
-                <Col sm="0.5">
-                  <FaTrashAlt className="trash"></FaTrashAlt>
-                </Col>
-              </Row>
-            </ListGroup.Item>
-            {/* Task 3 */}
-            <ListGroup.Item>
-              <Row>
-                <Col>
-                  <Form.Check type="checkbox" label="Call Mary" />
-                </Col>
-
-                <Col sm="auto">
-                  <FaPencilAlt className="pencil"></FaPencilAlt>
-                </Col>
-
-                <Col sm={0.5}>
-                  <FaTrashAlt className="trash"></FaTrashAlt>
-                </Col>
-              </Row>
-            </ListGroup.Item>
-          </ListGroup>
-          {/* Bottom plus */}
-          <AiFillPlusCircle
-            className="plusButton"
-            color="green"
-          ></AiFillPlusCircle>
-        </Col>
+        <MainContent/>
+        
       </Row>
     </Container>
 
--- biglab1/src/components/MainContent.js
+++ biglab1/src/components/MainContent.js
@@ -1,1 +1,79 @@
-//mainContent
+import { Col, Row, Container, Form, ListGroup } from 'react-bootstrap';
+import { FaTrashAlt, FaPencilAlt } from "react-icons/fa";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { BiUserCircle } from "react-icons/bi";
+
+function MainContent(props){
+    return(
+        <Col as={Container} fluid="xl" className="mainContainer below-nav">
+          <h1 id="selectedFilter">
+            <b>Filter</b>: all
+          </h1>
+          <ListGroup variant="flush">
+            {/* Task 1 */}
+            <ListGroup.Item>
+              <Row>
+                <Col>
+                  <Form.Check type="checkbox" label="Study for the exam" />
+                </Col>
+
+                <Col sm="auto">
+                  <FaPencilAlt className="pencil"></FaPencilAlt>
+                </Col>
+
+                <Col sm={0.5}>
+                  <FaTrashAlt className="trash"></FaTrashAlt>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            {/* Taks2 */}
+            <ListGroup.Item>
+              <Row>
+                <Col>
+                  <Form.Check
+                    type="checkbox"
+                    label="Prepare the slides for the exam"
+                  />
+                </Col>
+                <Col sm="2">
+                  <BiUserCircle size="1.85em"></BiUserCircle>
+                </Col>
+
+                <Col>Saturday, June 20th 2020, 12:00:00 am</Col>
+
+                <Col sm="auto">
+                  <FaPencilAlt className="pencil"></FaPencilAlt>
+                </Col>
+
+                <Col sm="0.5">
+                  <FaTrashAlt className="trash"></FaTrashAlt>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            {/* Task 3 */}
+            <ListGroup.Item>
+              <Row>
+                <Col>
+                  <Form.Check type="checkbox" label="Call Mary" />
+                </Col>
+
+                <Col sm="auto">
+                  <FaPencilAlt className="pencil"></FaPencilAlt>
+                </Col>
+
+                <Col sm={0.5}>
+                  <FaTrashAlt className="trash"></FaTrashAlt>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+          </ListGroup>
+          {/* Bottom plus */}
+          <AiFillPlusCircle
+            className="plusButton"
+            color="green"
+          ></AiFillPlusCircle>
+        </Col>
+    );
+}
+
+export default MainContent;
