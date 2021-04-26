4547e5f41a0f4f55fafb3c831a63ed341683cbbf
JonnyMarsiano <jonnymarsiano@gmail.com>
2021-04-26 13:32:29 +0200

    Prove con altri task

--- biglab1/src/App.js
+++ biglab1/src/App.js
@@ -15,12 +15,16 @@ import { Task, List } from './TaskListCreate';
 const t1 = new Task(1, "laundry", false, true);
 const t2 = new Task(2, "monday lab", false, false, "2021-03-16T09:00:00.000Z");
 const t3 = new Task(3, "phone call", true, false, "2021-03-08T15:20:00.000Z");
+const t4 = new Task(4, "lab", true, false, "2021-04-26T15:20:00.000Z");
+const t5 = new Task(5, "study", false, true, "2021-04-27T15:20:00.000Z");
 
 // create the task list and add the dummy tasks
 const taskList = new List();
 taskList.add(t1);
 taskList.add(t2);
 taskList.add(t3);
+taskList.add(t4);
+taskList.add(t5);
 
 const filterNames = ["All", "Important", "Today", "Next7", "Private"];
 
