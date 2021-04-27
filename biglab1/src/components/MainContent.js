90a11923ca94f83009d911d0c59cade52a7b5af6
Daniele R.C <darthreca@gmail.com>
2021-04-28 00:15:34 +0200

    Added simple validation

--- biglab1/src/components/MainContent.js
+++ biglab1/src/components/MainContent.js
@@ -1,8 +1,11 @@
 import { Col, Container, Modal, Button, Form } from "react-bootstrap";
 import { AiFillPlusCircle } from "react-icons/ai";
 import TaskList from "./TaskList";
-import { useState } from 'react';
-import dayjs from "dayjs"
+import { useState } from "react";
+import dayjs from "dayjs";
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
+
+dayjs.extend(isSameOrAfter);
 
 function MainContent(props) {
   return (
@@ -11,7 +14,7 @@ function MainContent(props) {
         <b>Filter</b>: all
       </h1>
       <TaskList taskList={props.taskList} selected={props.selected}></TaskList>
-      <MyForm createElement = {props.createElement}></MyForm>
+      <MyForm createElement={props.createElement}></MyForm>
 
       {/* Bottom plus */}
       <AiFillPlusCircle className="plusButton" color="green"></AiFillPlusCircle>
@@ -19,60 +22,92 @@ function MainContent(props) {
   );
 }
 
-
 function MyForm(props) {
+  //State to show modal
   const [hidden, setHidden] = useState(true);
-
+  //Setters functions
   const Show = () => setHidden(false);
   const Hide = () => setHidden(true);
+  //Validate form
+  const ValidateForm = () => {
+    return description !== "" && date.isSameOrAfter(dayjs(), "date");
+  };
+  //Confirm adding a task
   const SubmitChanges = (event) => {
     event.preventDefault();
-    props.createElement(description, isUrgent,isPrivate, date);
+    if (!ValidateForm()) return;
+    props.createElement(description, isUrgent, isPrivate, date);
     Hide();
-  }
-
+  };
+  //Task paramaters
   const [date, setDate] = useState(dayjs());
-  const [description, setDescription] = useState('');
+  const [description, setDescription] = useState("");
   const [isUrgent, setUrgent] = useState(false);
   const [isPrivate, setPrivate] = useState(true);
 
-  
   return (
     <>
       <Button onClick={Show}>Add new task</Button>
-      < Modal show={!hidden} onHide={Hide}>
+
+      {/*Modal*/}
+
+      <Modal show={!hidden} onHide={Hide}>
         <Modal.Header closeButton>
           <Modal.Title>Insert Task</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
           <Form>
-            <Form.Group controlId='selectedTask'>
+            <Form.Group controlId="selectedTask">
               <Form.Label>Description</Form.Label>
-              <Form.Control type="text" placeholder="Description" value={description} onChange={(event) => setDescription(event.target.value)}/>
+              <Form.Control
+                type="text"
+                placeholder="Description"
+                defaultValue={description}
+                onChange={(event) => setDescription(event.target.value)}
+              />
             </Form.Group>
 
-            <Form.Group controlId='selectedDate'>
+            <Form.Group controlId="selectedDate">
               <Form.Label>Date</Form.Label>
-              <Form.Control type="date" value={date.format('YYYY-MM-DD')} onChange={(event) => setDate(dayjs(event.target.value))} />
+              <Form.Control
+                type="date"
+                defaultValue={date.format("YYYY-MM-DD")}
+                onChange={(event) => setDate(dayjs(event.target.value))}
+              />
             </Form.Group>
 
             <Form.Group controlId="formBasicCheckbox">
-              <Form.Check inline type="checkbox" checked={isPrivate} label="is private?" onChange={ (event) =>{ setPrivate(event.target.checked); } }/>
-              <Form.Check inline type="checkbox" checked={isUrgent} label="is urgent?" onChange={(event) => setUrgent(event.target.checked)}/>
+              <Form.Check
+                inline
+                type="checkbox"
+                checked={isPrivate}
+                label="is private?"
+                onChange={(event) => {
+                  setPrivate(event.target.checked);
+                }}
+              />
+              <Form.Check
+                inline
+                type="checkbox"
+                checked={isUrgent}
+                label="is urgent?"
+                onChange={(event) => setUrgent(event.target.checked)}
+              />
             </Form.Group>
           </Form>
         </Modal.Body>
 
         <Modal.Footer>
-          <Button onClick={Hide} variant="secondary">Close</Button>
-          <Button onClick={SubmitChanges} variant="primary">Save changes</Button>
+          <Button onClick={Hide} variant="secondary">
+            Close
+          </Button>
+          <Button onClick={SubmitChanges} variant="primary">
+            Save changes
+          </Button>
         </Modal.Footer>
-      </Modal >
-
+      </Modal>
     </>
-
-  )
-
+  );
 }
 export default MainContent;
